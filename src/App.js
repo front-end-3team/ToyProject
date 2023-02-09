8b96c0c1442e9767fe1e59022039ad80fc982f33
joayeon <whffu0156@gmail.com>
2023-02-10 00:33:08 +0900

    Design: Header 디자인 수정
    Header 디자인 수정하고 있습니다

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import "./App.css";
-import { MockPost } from "./__mocks__/post";
+import './App.css';
+import Header from './components/Header/Header';
+import { MockPost } from './__mocks__/post';
 
 function App() {
-  const Posts = MockPost(5);
-  /*MockPost 함수의 매개변수 count로 전달한 수 만큼 데이터가 생성됩니다*/
-  console.log(Posts);
+    const Posts = MockPost(5);
+    /*MockPost 함수의 매개변수 count로 전달한 수 만큼 데이터가 생성됩니다*/
+    console.log(Posts);
 
-  return <div className="App">:)</div>;
+    return (
+        <>
+            <Header />
+        </>
+    );
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,41 @@
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faPlus } from '@fortawesome/free-solid-svg-icons';
+import siteLogo from '../../assets/images/logo.png';
+
 function Header() {
     return (
-        <header>
-            <div>hamburger</div>
-            <div>
-                <div>logo</div>
-                <div>인스타고랭</div>
-            </div>
-            <div>+</div>
-        </header>
+        <Head>
+            <FontAwesomeIcon icon={faBars} />
+            <Site>
+                <div>
+                    <img src={siteLogo} alt="siteLogo.png" />
+                </div>
+                <span>Instagoraeng</span>
+            </Site>
+            <FontAwesomeIcon icon={faPlus} />
+        </Head>
     );
 }
 
 export default Header;
+
+const Head = styled.div`
+    display: flex;
+    justify-content: space-between;
+    background-color: #a3daff;
+`;
+
+const Site = styled.div`
+    display: flex;
+
+    & > div > img {
+        width: 30px;
+        height: 30px;
+    }
+
+    & span {
+        font-size: 20px;
+        font-family: 'googleKalam';
+    }
+`;

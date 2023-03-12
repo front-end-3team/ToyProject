953308b84e1225e08773631a2ce1d73f38166ade
taeginote <ab315601@naver.com>
2023-03-13 00:29:52 +0900

    Add : contents, comment수정 구현 완료

--- src/pages/PostList/components/Post/UnderBox/Contents/Contents.js
+++ src/pages/PostList/components/Post/UnderBox/Contents/Contents.js
@@ -3,56 +3,100 @@ import * as P from "../style";
 import { useState } from "react";
 
 function Contents({ postlist }) {
-    const [view, setview] = useState(false);
-    const [inputs, setInputs] = useState("");
-    const onChangeInputs = (e) => {
-        const { name, value } = e.target;
-        setInputs({ ...inputs, [name]: value });
-    };
-    const { contents, nickname, name } = inputs;
+  const [onEdit, setOnEdit] = useState(false);
+  const [onEditContent, setOnEditContent] = useState({
+    nick: "",
+    id: "",
+    content: "",
+  });
 
-    const onChangeContents = () => {
-        setview((prev) => !prev);
-        if (postlist.myComment === "N")
-            return alert("자신 댓글만 수정 가능합니다.");
-        if (postlist.myComment === "Y") {
-            postlist.content = contents;
-            postlist.User.nick_name = name;
-            postlist.User.id = nickname;
-        }
-    };
-    return (
-        <P.ContentsBox>
-            <P.Profile>
-                <Img src={postlist.User.profile_img} />
-                <div>{postlist.User.nick_name}</div>
-            </P.Profile>
+  const onContentEdit = () => {
+    if (postlist.myPost === "Y") {
+      setOnEdit((prev) => !prev);
+    } else alert("니거만 해");
+    postlist.User.nick_name = onEditContent.nick;
+    postlist.User.id = onEditContent.id;
+    postlist.content = onEditContent.content;
+    console.log(postlist);
+  };
 
-            <P.ContentsCenter>
-                <div>
-                    <div>
-                        {postlist.myComment === "Y" && view === true ? (
-                            <input onChange={onChangeInputs} name="name" />
-                        ) : (
-                            <div>{postlist.User.id}</div>
-                        )}
-                        {/* <div>{postlist.User.id}</div> */}
-                        <div>{String(postlist.createdAt)}</div>
-                    </div>
-                    <div>
-                        <button onClick={onChangeContents}>✏️️</button>
-                        <button>🗑️</button>
-                    </div>
-                </div>
+  const onChangeContent = (e) => {
+    const { name, value } = e.target;
+    setOnEditContent({ ...onEditContent, [name]: value });
+  };
 
-                <div>{postlist.content}</div>
-            </P.ContentsCenter>
-        </P.ContentsBox>
-    );
+  return (
+    <P.ContentsBox>
+      <P.Profile>
+        <Img src={postlist.User.profile_img} />
+        {onEdit ? (
+          <input name="nick" onChange={onChangeContent} />
+        ) : (
+          <div>{postlist.User.nick_name}</div>
+        )}
+      </P.Profile>
+
+      <P.ContentsCenter>
+        <div>
+          <div>
+            {onEdit ? (
+              <input name="id" onChange={onChangeContent} />
+            ) : (
+              <div>{postlist.User.id}</div>
+            )}
+            <div>{("" + postlist.createdAt).slice(0, 24)}</div>
+          </div>
+          <div>
+            {onEdit ? (
+              <button onClick={onContentEdit}>완료</button>
+            ) : (
+              <button onClick={onContentEdit}>✏️️</button>
+            )}
+            <button>🗑️</button>
+          </div>
+        </div>
+
+        {onEdit ? (
+          <textarea name="content" onChange={onChangeContent} />
+        ) : (
+          <div>{postlist.content}</div>
+        )}
+      </P.ContentsCenter>
+    </P.ContentsBox>
+  );
 }
+//   return (
+//     <P.ContentsBox>
+//       <P.Profile>
+//         <Img src={postlist.User.profile_img} />
+//         <div>{postlist.User.nick_name}</div>
+//       </P.Profile>
+
+//       <P.ContentsCenter>
+//         <div>
+//           <div>
+//             {postlist.myComment === "Y" && view === true ? (
+//               <input onChange={onChangeInputs} name="name" />
+//             ) : (
+//               <div>{postlist.User.id}</div>
+//             )}
+//             {/* <div>{postlist.User.id}</div> */}
+//             <div>{String(postlist.createdAt)}</div>
+//           </div>
+//           <div>
+//             <button onClick={onChangeContents}>✏️️</button>
+//             <button>🗑️</button>
+//           </div>
+//         </div>
+
+//         <div>{postlist.content}</div>
+//       </P.ContentsCenter>
+//     </P.ContentsBox>
+//   );
+// }
 
 export default Contents;
 
 const Img = styled.img`
-    border-radius: 50%;
+  border-radius: 50%;
 `;

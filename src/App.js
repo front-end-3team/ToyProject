6b4fed847c18ef526fb783c4588b5770cf0d57a7
taeginote <ab315601@naver.com>
2023-03-12 23:35:01 +0900

    ADD : slide, 수정, modal 추가 까지 구현

--- src/App.js
+++ src/App.js
@@ -8,24 +8,24 @@ import Index from "./pages/PostList/components/Post/index";
 import { MockPost } from "./__mocks__/post";
 
 function App() {
-    const Posts = MockPost(10);
-    const [posts, setPosts] = useState(Posts);
+  const Posts = MockPost(10);
+  const [posts, setPosts] = useState(Posts);
 
-    /*MockPost 함수의 매개변수 count로 전달한 수 만큼 데이터가 생성됩니다*/
+  /*MockPost 함수의 매개변수 count로 전달한 수 만큼 데이터가 생성됩니다*/
 
-    const { modalview } = useContext(modalContext);
-    console.log(posts);
-    return (
+  const { modalview } = useContext(modalContext);
+  console.log(posts);
+  return (
+    <>
+      {modalview && <Modal posts={posts} setPosts={setPosts} />}
+      <Header />
+      {posts.map((el) => (
         <>
-            {modalview && <Modal posts={posts} setPosts={setPosts} />}
-            <Header />
-            {posts.map((el) => (
-                <>
-                    <Index postlist={el} posts={posts} setPosts={setPosts} />
-                </>
-            ))}
+          <Index postlist={el} posts={posts} setPosts={setPosts} />
         </>
-    );
+      ))}
+    </>
+  );
 }
 
 export default App;
--- src/components/Slide.js
+++ src/components/Slide.js
@@ -9,22 +9,23 @@ import "./styles.css";
 
 // import required modules
 import { Navigation } from "swiper";
+import styled from "styled-components";
 
 function Slide({ postlist }) {
-    return (
-        <>
-            <Swiper
-                navigation={true}
-                modules={[Navigation]}
-                className="mySwiper"
-            >
-                {postlist.Post_img.map((el) => (
-                    <SwiperSlide>
-                        <img src={el} />
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        </>
-    );
+  return (
+    <>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        {postlist.Post_img.map((el) => (
+          <SwiperSlide>
+            <Img src={el} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
 }
+
+const Img = styled.img`
+  z-index: 0;
+`;
 export default Slide;
